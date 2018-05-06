ea763a463a92a8fee8bb5b13af9c25f96e7f1e7f
Brandon Braun <brandonbraun653@gmail.com>
2018-05-05 22:27:58 -0500

    Updated binary upload script testing sandbox

--- Thor/testing/stlink_experimentation.py
+++ Thor/testing/stlink_experimentation.py
@@ -1,12 +1,20 @@
 from pystlink import PyStlink
-from lib.stlinkusb import StlinkUsbConnector
-
+from lib import dbg
 
 
 if __name__ == '__main__':
-    #stlink = StlinkUsbConnector()
 
     test = PyStlink()
-    test.start()
+    test._dbg = dbg.Dbg(1)
+    test.detect_cpu(expected_cpus=['STM32F765xI', 'STM32F767xI', 'STM32F769xI',
+                                   'STM32F777xI', 'STM32F778xI', 'STM32F779xI'])
+    #test.start()
 
-    test.cmd(['flash', 'erase', 'verify', 'ChimeraDevelopment.bin'])
+    try:
+        test.cmd(['flash', 'erase', 'verify', '0x08000000', 'ChimeraDevelopment.bin'])
+
+    except:
+        pass
+
+    test.cmd(['write', '0x08000000', 'ChimeraDevelopment.bin'])
+    test.cmd(['reset'])
