0868a4dbdc5447df5f4c7ce5030cebe1399ca590
Brandon Braun <brandonbraun653@gmail.com>
2018-05-24 16:09:09 -0500

    Added test case for Jenkins automated builds

---
+++ Thor/testing/JenkinsTest/main.cpp
@@ -0,0 +1,78 @@
+/* FreeRTOS Includes */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Thor Includes */
+#include <Thor/include/thor.hpp>
+#include <Thor/include/config.hpp>
+#include <Thor/include/definitions.hpp>
+#include <Thor/include/gpio.hpp>
+
+/* Project Includes */
+#include "led_thread.hpp"
+#include "spi_thread.hpp"
+#include "serial_thread.hpp"
+
+/*
+Blue Led: PB7
+Red Led: PB14
+Green Led: PB0
+*/
+void redLED(void* arguments);
+void greenLED(void* arguments);
+
+int main(void)
+{
+	HAL_Init();
+	ThorInit();
+
+	xTaskCreate(redLED, "ledThread1", 350, NULL, 2, NULL);
+	xTaskCreate(greenLED, "ledThread2", 350, NULL, 2, NULL);
+	xTaskCreate(ledThread, "ledThread3", 350, NULL, 2, NULL);
+	//xTaskCreate(serialThread, "serialThread", 1000, NULL, 2, NULL);
+	vTaskStartScheduler();
+	
+	/* Should never reach here as scheduler should be running */
+	for (;;)
+	{
+		
+	}
+}
+
+void redLED(void* arguments)
+{
+	using namespace Thor::Peripheral::GPIO;
+	using namespace Thor::Definitions::GPIO;
+
+	GPIOClass red_led(GPIOB, PIN_14);
+	
+	red_led.mode(OUTPUT_PP);
+	red_led.write(HIGH);
+
+	
+	TickType_t lastTimeWoken = xTaskGetTickCount();
+	for (;;)
+	{
+		red_led.toggle();
+		vTaskDelayUntil(&lastTimeWoken, pdMS_TO_TICKS(100));
+	}
+}
+
+void greenLED(void* arguments)
+{
+	using namespace Thor::Peripheral::GPIO;
+	using namespace Thor::Definitions::GPIO;
+
+	GPIOClass green_led(GPIOB, PIN_0);
+	
+	green_led.mode(OUTPUT_PP);
+	green_led.write(HIGH);
+
+	
+	TickType_t lastTimeWoken = xTaskGetTickCount();
+	for (;;)
+	{
+		green_led.toggle();
+		vTaskDelayUntil(&lastTimeWoken, pdMS_TO_TICKS(1000));
+	}
+}
---
+++ Thor/testing/JenkinsTest/serial_thread.cpp
@@ -0,0 +1,45 @@
+#include "serial_thread.hpp"
+
+/* C/C++ Includes */
+#include <string>
+
+/* Boost Includes */
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+/* Thor Includes */
+#include <Thor/include/definitions.hpp>
+#include <Thor/include/uart.hpp>
+
+using namespace Thor::Definitions::Serial;
+
+void serialThread(void* argument)
+{
+	using namespace Thor::Peripheral::UART;
+
+	UARTClass_sPtr uart = UARTClass::create(4);
+
+	uart->begin(SERIAL_BAUD_115200);
+	uart->setMode(TX, BLOCKING);
+
+	const char* test1 = "Hey mate.\r\n";
+	const char* test2 = "Testing testing!\r\n";
+	const char* test3 = "I love watermelon.\r\n";
+
+	/* Inform the init task that everything has been set up */
+	// xTaskSendMessage(INIT_THREAD, 1u);
+	// vTaskSuspend(NULL);
+	// taskYIELD();
+
+
+	uart->setMode(TX, BLOCKING);
+	TickType_t lastTimeWoken = xTaskGetTickCount();
+	for (;;)
+	{
+		uart->write(test1);
+		uart->write(test2);
+		uart->write(test3);
+
+		vTaskDelayUntil(&lastTimeWoken, pdMS_TO_TICKS(500));
+	}
+}
