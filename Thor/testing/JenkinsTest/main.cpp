5b35aa0c436238cd03b725671b4c57c34046e13f
Brandon Braun <brandonbraun653@gmail.com>
2018-05-25 11:17:49 -0500

    Please jenkins work this time...

--- Thor/testing/JenkinsTest/main.cpp
+++ Thor/testing/JenkinsTest/main.cpp
@@ -26,7 +26,7 @@ int main(void)
 	HAL_Init();
 	ThorInit();
 
-	//yoooos
+	//Please please Jenkins work...
 
 	xTaskCreate(redLED, "ledThread1", 350, NULL, 2, NULL);
 	xTaskCreate(greenLED, "ledThread2", 350, NULL, 2, NULL);
