67e3aa0f08ee8f485a7c9aa522d0ce40076a5194
Brandon Braun <brandonbraun653@gmail.com>
2018-09-16 16:17:56 -0500

    Added a significant number of cmake helper functions to support common stm32 tasks. Updated the gitmodules with new refs. Updated the toolchain.cmake file to run more cleanly.

---
+++ build/CMakeFiles/thor.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/git/Microcontrollers/Thor_STM32/main.cpp" "C:/git/Microcontrollers/Thor_STM32/build/CMakeFiles/thor.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STM32F446xx"
+  "USE_FULL_LL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Thor/lib/stm32hal_f4/STM32F4xx_HAL_Driver/Inc"
+  "../Thor/lib/stm32hal_f4/Device/conf"
+  "../Thor/lib/stm32hal_f4/CMSIS/Include"
+  "../Thor/lib/stm32hal_f4/CMSIS/Device/ST/STM32F4xx/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
