d0c2f338f313cd50a84c388ec1ac6704e48a4bcc
Brandon Braun <brandonbraun653@gmail.com>
2018-09-16 16:17:56 -0500

    Added a significant number of cmake helper functions to support common stm32 tasks. Updated the gitmodules with new refs. Updated the toolchain.cmake file to run more cleanly.

---
+++ build/CMakeFiles/thor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thor.dir/main.cpp.obj"
+  "libthor_dbg.pdb"
+  "libthor_dbg.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
