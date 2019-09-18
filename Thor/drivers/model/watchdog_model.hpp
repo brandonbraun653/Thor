7f60c31f3b550221b75fc6011c144b50875be6ae
Brandon Braun <brandonbraun653@gmail.com>
2019-09-18 17:54:25 -0500

    Added some function documentation

--- Thor/drivers/model/watchdog_model.hpp
+++ Thor/drivers/model/watchdog_model.hpp
@@ -30,10 +30,47 @@ namespace Thor::Driver::Watchdog
   public:
     virtual ~Basic() = default;
 
+    /**
+     *  Sets the closest available timeout that can be achieved
+     *  given the current hardware configuration.
+     *  
+     *  @param[in]  ms            The number of milliseconds to set the timeout to
+     *  @return Chimera::Status_t
+     */
+    virtual Chimera::Status_t setTimeout( const size_t ms );
+    
+    /**
+     *  Places the watchdog hardware in a state such that it needs to be 
+     *  periodically refereshed with the kick() function, otherwise causing
+     *  a system reset to occur.
+     *
+     *  @return void
+     */
+    virtual void start() = 0;
+
+    /**
+     *  Refreshes the watchdog counter so that it does not reset the board
+     *  
+     *  @return void
+     */
     virtual void kick() = 0;
 
+    /** 
+     *  Calculates the maximum delay (mS) that can be achieved with the
+     *  given prescaler value.
+     *  
+     *  @param[in]  prescaler     The watchdog clock prescaler
+     *  @return size_t            Millisecond delay
+     */
     virtual size_t maxDelay( const uint32_t prescaler ) = 0;
 
+    /** 
+     *  Calculates the minimum delay (mS) that can be achieved with the
+     *  given prescaler value.
+     *  
+     *  @param[in]  prescaler     The watchdog clock prescaler
+     *  @return size_t            Millisecond delay
+     */
     virtual size_t minDelay( const uint32_t prescaler ) = 0;
   };
 }    // namespace Thor::Driver::Watchdog
