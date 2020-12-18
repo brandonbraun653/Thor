625006dc70e48152a7a3b300e82d3f3a2694646d
brandonbraun653 <brandonbraun653@gmail.com>
2020-12-17 22:59:43 -0600

    Add optional user config file compiler switch

--- Thor/config/config.hpp
+++ Thor/config/config.hpp
@@ -3,8 +3,8 @@
  *    config.hpp
  *
  *  Description:
- *    Sources configuration settings from the appropriate file. Try not to modify
- *    this too often as it will trigger a complete rebuild.
+ *    Provides system configuration settings by routing to the correct
+ *    header file. Usually these files enable/disable peripheral modules.
  *
  *  2020 | Brandon Braun | brandonbraun653@gmail.com
  *******************************************************************************/
@@ -16,6 +16,12 @@
 /* Thor Includes */
 #include <Thor/hld/common/preprocessor.hpp>
 
+/*-------------------------------------------------------------------------------
+Select the appropriate driver configuration file
+-------------------------------------------------------------------------------*/
+#if defined( THOR_USER_CONFIG )
+#include "thor_user_config.hpp"
+#else /* Default Project Configuration */
 #if !defined( TARGET_LLD_MOCK ) && ( defined( TARGET_LLD_TEST ) || defined( _SIM ) )
 #include <Thor/config/simulator/thor_sim_config.hpp>
 #elif defined( TARGET_STM32F4 )
@@ -25,7 +31,8 @@
 #elif defined( TARGET_LLD_MOCK )
 #include <Thor/config/simulator/thor_gtest_config.hpp>
 #else
-#pragma message("Unknown configuration options")
+#pragma message( "Unknown configuration options" )
 #endif
+#endif /* THOR_USER_CONFIG */
 
 #endif /* !THOR_CONFIG_HPP */
