a78c9578c74c9975ba36d91cde4bb754af4fc3bf
Brandon Braun <brandonbraun653@protonmail.com>
2023-06-15 12:57:49 -0700

    Add interface file

---
+++ lld/interface/sdio/sdio_intf.hpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+ *  File Name:
+ *    sdio_intf.hpp
+ *
+ *  Description:
+ *    SDIO LLD Interface
+ *
+ *  2023 | Brandon Braun | brandonbraun653@protonmail.com
+ *****************************************************************************/
+
+#pragma once
+#ifndef THOR_LLD_SDIO_INTF_HPP
+#define THOR_LLD_SDIO_INTF_HPP
+
+/*-----------------------------------------------------------------------------
+Includes
+-----------------------------------------------------------------------------*/
+
+
+namespace Thor::LLD::SDIO
+{
+	
+}    // namespace Thor::LLD::SDIO
+
+#endif /* !THOR_LLD_SDIO_INTF_HPP */
